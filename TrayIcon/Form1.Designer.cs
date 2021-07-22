ce87db0f60c357017869f68b3f1faee1e19869a7
Pierre Pic <pierre.pic63122@gmail.com>
2021-07-22 15:27:10 +0200

    Add files via upload

---
+++ TrayIcon/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿
+namespace TrayIcon
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.playToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pauseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nextToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.previousToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Play = new System.Windows.Forms.Button();
+            this.next = new System.Windows.Forms.Button();
+            this.previous = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "notifyIcon1";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.contextMenuStrip1.Font = new System.Drawing.Font("Segoe UI Semilight", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.playToolStripMenuItem,
+            this.pauseToolStripMenuItem,
+            this.nextToolStripMenuItem,
+            this.previousToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.contextMenuStrip1.ShowImageMargin = false;
+            this.contextMenuStrip1.Size = new System.Drawing.Size(108, 100);
+            // 
+            // playToolStripMenuItem
+            // 
+            this.playToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.playToolStripMenuItem.Image = global::TrayIcon.Properties.Resources.outline_play_arrow_black_48dp;
+            this.playToolStripMenuItem.Name = "playToolStripMenuItem";
+            this.playToolStripMenuItem.Size = new System.Drawing.Size(107, 24);
+            this.playToolStripMenuItem.Text = "Play";
+            // 
+            // pauseToolStripMenuItem
+            // 
+            this.pauseToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.pauseToolStripMenuItem.Name = "pauseToolStripMenuItem";
+            this.pauseToolStripMenuItem.Size = new System.Drawing.Size(107, 24);
+            this.pauseToolStripMenuItem.Text = "Pause";
+            // 
+            // nextToolStripMenuItem
+            // 
+            this.nextToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.nextToolStripMenuItem.Name = "nextToolStripMenuItem";
+            this.nextToolStripMenuItem.Size = new System.Drawing.Size(107, 24);
+            this.nextToolStripMenuItem.Text = "Next";
+            // 
+            // previousToolStripMenuItem
+            // 
+            this.previousToolStripMenuItem.ForeColor = System.Drawing.Color.White;
+            this.previousToolStripMenuItem.Name = "previousToolStripMenuItem";
+            this.previousToolStripMenuItem.Size = new System.Drawing.Size(107, 24);
+            this.previousToolStripMenuItem.Text = "Previous";
+            // 
+            // Play
+            // 
+            this.Play.BackColor = System.Drawing.Color.Transparent;
+            this.Play.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Play.ForeColor = System.Drawing.Color.Transparent;
+            this.Play.Image = global::TrayIcon.Properties.Resources.outline_play_arrow_black_48dp;
+            this.Play.Location = new System.Drawing.Point(170, 12);
+            this.Play.Name = "Play";
+            this.Play.Size = new System.Drawing.Size(79, 80);
+            this.Play.TabIndex = 1;
+            this.Play.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.Play.UseVisualStyleBackColor = false;
+            this.Play.Click += new System.EventHandler(this.playClick);
+            // 
+            // next
+            // 
+            this.next.BackColor = System.Drawing.Color.Transparent;
+            this.next.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.next.ForeColor = System.Drawing.Color.Transparent;
+            this.next.Image = ((System.Drawing.Image)(resources.GetObject("next.Image")));
+            this.next.Location = new System.Drawing.Point(255, 27);
+            this.next.Name = "next";
+            this.next.Size = new System.Drawing.Size(50, 51);
+            this.next.TabIndex = 2;
+            this.next.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.next.UseVisualStyleBackColor = false;
+            this.next.Click += new System.EventHandler(this.next_Click);
+            // 
+            // previous
+            // 
+            this.previous.BackColor = System.Drawing.Color.Transparent;
+            this.previous.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.previous.ForeColor = System.Drawing.Color.Transparent;
+            this.previous.Image = ((System.Drawing.Image)(resources.GetObject("previous.Image")));
+            this.previous.Location = new System.Drawing.Point(114, 27);
+            this.previous.Name = "previous";
+            this.previous.Size = new System.Drawing.Size(50, 51);
+            this.previous.TabIndex = 3;
+            this.previous.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.previous.UseVisualStyleBackColor = false;
+            this.previous.Click += new System.EventHandler(this.previous_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.Transparent;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(12, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 51);
+            this.button1.TabIndex = 4;
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.ReduceScreen);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(414, 104);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.previous);
+            this.Controls.Add(this.next);
+            this.Controls.Add(this.Play);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem playToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pauseToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nextToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem previousToolStripMenuItem;
+        private System.Windows.Forms.Button Play;
+        private System.Windows.Forms.Button next;
+        private System.Windows.Forms.Button previous;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
